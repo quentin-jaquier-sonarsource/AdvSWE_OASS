1ca4933020027e59994c63899762fc275717199c
Alexandra <alexandra@MacBook-Air-75.local>
2022-09-26 01:24:56 -0400

    Initial Commit for the Movies!

---
+++ src/main/java/coms/w4156/moviewishlist/MovieWishlistApplication.java
@@ -0,0 +1,13 @@
+package coms.w4156.moviewishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieWishlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coms/w4156/moviewishlist/MovieWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package coms.w4156.moviewishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
