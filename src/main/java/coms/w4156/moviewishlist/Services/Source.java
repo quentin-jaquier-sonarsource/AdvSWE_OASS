28d922369bfcd31fde2ac1d2ab5ee54886b54b25
SachinJay <sjd232@cornell.edu>
2022-10-11 16:41:38 -0400

    Compressed Source class using Lombok

--- src/main/java/coms/w4156/moviewishlist/Services/Source.java
+++ src/main/java/coms/w4156/moviewishlist/Services/Source.java
@@ -1,8 +1,16 @@
 package coms.w4156.moviewishlist.Services;
 
+import lombok.*;
+
 /**
  * Class to represent a WatchMode streaming source
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Source {
     private int source_id;
     private String name;
@@ -16,98 +24,7 @@ public class Source {
     private int seasons;
     private int episodes;
 
-    public Source() {
-    }
-
     public boolean isFreeWithSubscription() {
         return this.type.equals("sub");
-    }
-
-    public int getSource_id() {
-        return source_id;
-    }
-
-    public void setSource_id(int source_id) {
-        this.source_id = source_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getIos_url() {
-        return ios_url;
-    }
-
-    public void setIos_url(String ios_url) {
-        this.ios_url = ios_url;
-    }
-
-    public String getAndroid_url() {
-        return android_url;
-    }
-
-    public void setAndroid_url(String android_url) {
-        this.android_url = android_url;
-    }
-
-    public String getWeb_url() {
-        return web_url;
-    }
-
-    public void setWeb_url(String web_url) {
-        this.web_url = web_url;
-    }
-
-    public String getFormat() {
-        return format;
-    }
-
-    public void setFormat(String format) {
-        this.format = format;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getSeasons() {
-        return seasons;
-    }
-
-    public void setSeasons(int seasons) {
-        this.seasons = seasons;
-    }
-
-    public int getEpisodes() {
-        return episodes;
-    }
-
-    public void setEpisodes(int episodes) {
-        this.episodes = episodes;
     }
 }
