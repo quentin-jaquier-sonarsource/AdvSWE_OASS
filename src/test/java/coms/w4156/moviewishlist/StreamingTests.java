bd904e07648d3f9cefd9719b45546808b1b955ff
SachinJay <sjd232@cornell.edu>
2022-10-11 16:10:25 -0400

    Added first test for the watchmode service

--- src/test/java/coms/w4156/moviewishlist/StreamingTests.java
+++ src/test/java/coms/w4156/moviewishlist/StreamingTests.java
@@ -1,20 +1,61 @@
 package coms.w4156.moviewishlist;
 
 import coms.w4156.moviewishlist.Controllers.StreamingController;
+import coms.w4156.moviewishlist.Services.Source;
+import coms.w4156.moviewishlist.Services.WatchModeService;
+import coms.w4156.moviewishlist.utils.Config;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
-@WebMvcTest(StreamingController.class)
+@ExtendWith(MockitoExtension.class)
 public class StreamingTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+    Config config = new Config();
+    private final String skyfallId = "1350564";
+    private final String watchmode_url = "https://api.watchmode.com/v1/title/"+skyfallId+"/sources/?apiKey="+config.getAPIKey();
+
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private WatchModeService wms = new WatchModeService("prod");
 
     @Test
     public void testAvailable() {
-//        TODO
+        Source netflix = new Source();
+        netflix.setName("Netflix");
+        netflix.setType("sub");
+
+        Source amazon = new Source();
+        amazon.setName("Amazon");
+        amazon.setType("sub");
+
+        Source vudu = new Source();
+        vudu.setName("VUDU");
+        vudu.setType("buy");
+
+        Source[] sourcesAvailableFreeWithSubscription = new Source[]{netflix, amazon};
+        Source[] allSources = new Source[]{netflix, amazon, vudu};
+
+        Mockito
+                .when(restTemplate.getForEntity(watchmode_url, Source[].class))
+                .thenReturn(new ResponseEntity(allSources, HttpStatus.OK));
+
+        String[] returnedSources = wms.getResponse();
+
+        Assertions.assertEquals(2, returnedSources.length);
     }
 
 }
