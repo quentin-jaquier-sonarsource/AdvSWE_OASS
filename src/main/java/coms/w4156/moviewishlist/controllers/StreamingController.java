a0dc410faa4d10a1ec7e0b28ab76d662f8bec4cd
SachinJay <sjd232@cornell.edu>
2022-10-22 15:58:57 -0400

    fixed checkstyle errors

--- src/main/java/coms/w4156/moviewishlist/controllers/StreamingController.java
+++ src/main/java/coms/w4156/moviewishlist/controllers/StreamingController.java
@@ -2,7 +2,11 @@ package coms.w4156.moviewishlist.controllers;
 
 import coms.w4156.moviewishlist.services.WatchModeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
