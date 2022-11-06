93909c28c183cf109f5c20e417fb84fb3af1a17a
SachinJay <sjd232@cornell.edu>
2022-11-06 15:40:47 -0500

    re-enabled contextLoads test

--- src/test/java/coms/w4156/moviewishlist/MovieWishlistApplicationTests.java
+++ src/test/java/coms/w4156/moviewishlist/MovieWishlistApplicationTests.java
@@ -1,13 +1,11 @@
 package coms.w4156.moviewishlist;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovieWishlistApplicationTests {
 
-    @Disabled
     @Test
     void contextLoads() {
     }
