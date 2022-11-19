7c7d897993549754c95cf7daaba310785b08c55d
Alexandra Holguin <anh2150@columbia.edu>
2022-11-19 16:01:49 -0500

    clean up schema before adding clients (#36)

--- src/main/java/coms/w4156/moviewishlist/controllers/GraphqlController.java
+++ src/main/java/coms/w4156/moviewishlist/controllers/GraphqlController.java
@@ -5,16 +5,19 @@ import coms.w4156.moviewishlist.models.User;
 import coms.w4156.moviewishlist.models.watchMode.TitleDetail;
 import coms.w4156.moviewishlist.models.watchMode.TitleSearchResult;
 import coms.w4156.moviewishlist.models.watchMode.WatchModeNetwork;
-import coms.w4156.moviewishlist.models.watchMode.WatchModeSource;
 import coms.w4156.moviewishlist.services.MovieService;
 import coms.w4156.moviewishlist.services.UserService;
 import coms.w4156.moviewishlist.services.WatchModeService;
 import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.DataFetchingFieldSelectionSet;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -80,14 +83,62 @@ public class GraphqlController {
     }
 
     /**
-     * Get all WatchMode sources.
-     *
-     * @return List of User objects
+     * Get the details for this title using the /title API.
+     * @param searchResult - The search result to get the details for.
+     * @param env - The GraphQL environment
+     * @return - The title details
      */
-    @QueryMapping
-    public Collection<WatchModeSource> sources() {
-        return watchModeService.getAllSources();
+    @SchemaMapping(typeName = "TitleSearchResult", field = "details")
+    public TitleDetail getDetails(
+        final TitleSearchResult searchResult,
+        final DataFetchingEnvironment env
+    ) {
+        Set<String> knownFields = Set.of(
+            "id",
+            "title",
+            "type",
+            "year",
+            "tmdbId",
+            "tmdbType",
+            "poster"
+        );
+
+        long unknownFields = env
+            .getSelectionSet()
+            .getFields()
+            .stream()
+            .filter(el -> !knownFields.contains(el.getName()))
+            .count();
+
+        Boolean includeSources = env.getSelectionSet().contains("sources");
+
+        if (unknownFields > 0) {
+            return watchModeService.getTitleDetail(
+                searchResult.getId().toString(),
+                includeSources
+            );
+        }
+
+        TitleDetail detail = new TitleDetail();
+        detail.setId(searchResult.getId());
+        detail.setTitle(searchResult.getName());
+        detail.setType(searchResult.getType());
+        detail.setYear(searchResult.getYear());
+        detail.setTmdbId(searchResult.getTmdbId());
+        detail.setTmdbType(searchResult.getTmdbType());
+        detail.setPoster(searchResult.getImageUrl());
+        return detail;
     }
+
+    // /**
+    //  * Get all WatchMode sources.
+    //  *
+    //  * @return List of User objects
+    //  */
+    // @QueryMapping
+    // public Collection<WatchModeSource> sources() {
+    //     return watchModeService.getAllSources();
+    // }
 
     /**
      * Get all WatchMode networks.
@@ -114,5 +165,45 @@ public class GraphqlController {
     ) {
         Boolean includeSources = env.getSelectionSet().contains("sources");
         return watchModeService.getTitleDetail(id, includeSources);
+    }
+
+    /**
+     * Get title details by id.
+     *
+     * @param titledetail - The TitleDetail object
+     * @param env - The DataFetchingEnvironment
+     *
+     * @return Details of the Title
+     */
+    @SchemaMapping(typeName = "TitleDetail", field = "similarTitles")
+    public List<TitleDetail> similarTitles(
+        final TitleDetail titledetail,
+        final DataFetchingEnvironment env
+    ) {
+        DataFetchingFieldSelectionSet selectionSet = env.getSelectionSet();
+
+        if (
+            selectionSet.contains("id") && selectionSet.getFields().size() == 1
+        ) {
+            return titledetail
+                .getSimilarTitlesIds()
+                .stream()
+                .map(id -> {
+                    TitleDetail detail = new TitleDetail();
+                    detail.setId(id);
+                    return detail;
+                })
+                .toList();
+        }
+
+        Boolean hasSources = env.getSelectionSet().contains("sources");
+
+        return titledetail
+            .getSimilarTitlesIds()
+            .stream()
+            .map(id ->
+                watchModeService.getTitleDetail(id.toString(), hasSources)
+            )
+            .toList();
     }
 }
