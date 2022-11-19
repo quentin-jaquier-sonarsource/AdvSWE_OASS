49e49c60f4070faae1e36c958d9ae5c5e0d5e7e6
Alexandra Holguin <anh2150@columbia.edu>
2022-11-19 16:01:49 -0500

    clean up schema before adding clients (#36)

--- src/main/java/coms/w4156/moviewishlist/models/watchMode/TitleDetail.java
+++ src/main/java/coms/w4156/moviewishlist/models/watchMode/TitleDetail.java
@@ -1,34 +1,78 @@
 package coms.w4156.moviewishlist.models.watchMode;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import java.util.List;
+import lombok.Data;
 
-public record TitleDetail(
-    Long id,
-    String title,
-    String original_title,
-    String plot_overview,
-    String type,
-    Integer runtime_minutes,
-    Integer year,
-    Integer end_year,
-    String release_date,
-    String imdb_id,
-    Long tmdb_id,
-    String tmdb_type,
-    List<Long> genres,
-    List<String> genre_names,
-    double user_rating,
-    Integer critic_score,
-    String us_rating,
-    String poster,
-    String backdrop,
-    String original_language,
-    List<Long> similar_titles,
-    List<Long> networks,
-    List<String> network_names,
-    String trailer,
-    String trailer_thumbnail,
-    double relevance_percentile,
-    // TODO: This isn't working as expected
-    List<WatchModeSource> sources
-) {}
+@Data
+public class TitleDetail {
+
+    private Long id;
+    private String title;
+
+    @JsonAlias("original_title")
+    private String originalTitle;
+
+    @JsonAlias("plot_overview")
+    private String plotOverview;
+
+    private String type;
+
+    @JsonAlias("runtime_minutes")
+    private Integer runtimeMinutes;
+
+    private Integer year;
+
+    @JsonAlias("end_year")
+    private Integer endYear;
+
+    @JsonAlias("release_date")
+    private String releaseDate;
+
+    @JsonAlias("imdb_id")
+    private String imdbId;
+
+    @JsonAlias("tmdb_id")
+    private Long tmdbId;
+
+    @JsonAlias("tmdb_type")
+    private String tmdbType;
+
+    private List<Long> genres;
+
+    @JsonAlias("genre_names")
+    private List<String> genreNames;
+
+    @JsonAlias("user_rating")
+    private double userRating;
+
+    @JsonAlias("critic_score")
+    private Integer criticScore;
+
+    @JsonAlias("us_rating")
+    private String usRating;
+
+    private String poster;
+    private String backdrop;
+
+    @JsonAlias("original_language")
+    private String originalLanguage;
+
+    @JsonAlias("similar_titles")
+    private List<Long> similarTitlesIds;
+
+    private List<Long> networks;
+
+    @JsonAlias("network_names")
+    private List<String> networkNames;
+
+    private String trailer;
+
+    @JsonAlias("trailer_thumbnail")
+    private String trailerThumbnail;
+
+    @JsonAlias("relevance_percentile")
+    private double relevancePercentile;
+
+    private List<WatchModeSource> sources;
+}
