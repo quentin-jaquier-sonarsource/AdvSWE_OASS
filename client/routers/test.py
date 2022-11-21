0d35fa1f6536dd6c6c5ef5f2ab7bcdfbb98b70bb
SachinJay <sjd232@cornell.edu>
2022-11-21 16:47:42 -0500

    added a test endpoint to test graphql endpoint

--- client/routers/test.py
+++ client/routers/test.py
@@ -1,6 +1,7 @@
+import json
 from fastapi import APIRouter
 from requests import Response
-from constants import TEST_URL, TEST_TAG
+from constants import GRAPHQL_URL, TEST_URL, TEST_TAG
 
 import requests
 
@@ -25,3 +26,26 @@ async def test():
 
         # If there was an issue hitting the test endpoint, show it to the user
         return {"Exception: ": e.__str__()}
+
+@router.get("/graphql")
+async def graphql():
+    """
+    This endpoint hits a graphql endpoint. Arbitrarily, it queries for movies,
+    but the main point of this endpoint is just to hit our graphql endpoint
+    and make sure that everything is up and running
+    """
+
+    query = """query{
+        movies {
+            id
+        }
+    }
+    """
+
+    r : Response = requests.post(GRAPHQL_URL, json={'query' : query})
+
+    json_data = json.loads(r.text)["data"]
+    return {
+        "Status code" : r.status_code,
+        "Json Data" : json_data
+    }
