59eb4a9ab3d65bf1305068f0ee7e13decd96de5e
SachinJay <sjd232@cornell.edu>
2022-11-27 16:38:52 -0500

    Added tests for the /sources endpoitns

--- client/tests/test_constants.py
+++ client/tests/test_constants.py
@@ -3,3 +3,10 @@ DB_USER_NAME_ENV_VAR = "DB_USER_NAME"
 DB_PASSWORD_ENV_VAR = "DB_PASSWORD"
 
 CLIENT_EMAIL = "test@test.com"
+
+SOURCES_PREFIX = "/sources"
+
+# Watchmode id for the movie Host
+HOST_ID = 1616666
+
+HOST_TITLE = "Host"
