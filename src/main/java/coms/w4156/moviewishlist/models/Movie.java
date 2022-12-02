f72f0a7a8fa2aa9e731a384e375279cb194d0e36
omniyyahOS1 <omniayahya223@gmail.com>
2022-12-02 02:31:07 -0500

    Starting GraphqlControllerTest

--- src/main/java/coms/w4156/moviewishlist/models/Movie.java
+++ src/main/java/coms/w4156/moviewishlist/models/Movie.java
@@ -50,7 +50,7 @@ public class Movie implements ModelInterface<Long> {
     @Setter
     @Column(
             name = "movie_name",
-            nullable = false,
+            nullable = true,
             unique = false,
             updatable = false
     )
