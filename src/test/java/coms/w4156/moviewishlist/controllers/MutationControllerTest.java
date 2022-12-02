cee3cc808a561dba70f23d14ab6a66c32b1d2e09
omniyyahOS1 <omniayahya223@gmail.com>
2022-12-02 03:22:51 -0500

    fixing error and starting MutationControllerTest

--- src/test/java/coms/w4156/moviewishlist/controllers/MutationControllerTest.java
+++ src/test/java/coms/w4156/moviewishlist/controllers/MutationControllerTest.java
@@ -1,4 +1,37 @@
+package coms.w4156.moviewishlist.controllers;
+
+import coms.w4156.moviewishlist.models.Client;
+import coms.w4156.moviewishlist.models.Profile;
+import coms.w4156.moviewishlist.services.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@Import({ClientService.class,
+        MovieService.class,
+        WatchModeService.class,
+        WishlistService.class,
+        ProfileService.class})
+@GraphQlTest(GraphqlController.class)
 class MutationControllerTest {
-  
+
+    @Autowired
+    GraphQlTester graphQlTester;
+
+    @Test
+    void createClient() {
+    }
+
+    @Test
+    void updateClientTest() {
+    }
+
+    @Test
+    void deleteClientTest() {
+    }
+
 }
