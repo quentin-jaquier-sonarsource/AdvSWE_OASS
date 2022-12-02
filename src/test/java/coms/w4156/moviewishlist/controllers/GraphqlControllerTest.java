8faa0aa78717e274b0f3c8149fec5b67eb88cb95
omniyyahOS1 <omniayahya223@gmail.com>
2022-12-02 03:22:17 -0500

    fixing error

--- src/test/java/coms/w4156/moviewishlist/controllers/GraphqlControllerTest.java
+++ src/test/java/coms/w4156/moviewishlist/controllers/GraphqlControllerTest.java
@@ -3,30 +3,40 @@ package coms.w4156.moviewishlist.controllers;
 import coms.w4156.moviewishlist.models.Client;
 import coms.w4156.moviewishlist.models.Profile;
 import coms.w4156.moviewishlist.services.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/* Integration Test */
-
-@Import({ClientService.class,
-        MovieService.class,
-        WatchModeService.class,
-        WishlistService.class,
-        ProfileService.class})
 @GraphQlTest(GraphqlController.class)
 class GraphqlControllerTest {
 
     @Autowired
     GraphQlTester graphQlTester;
 
-    @Test
-    void getAllClientsTest(){
+    @MockBean
+    ClientService clientService;
 
+    @MockBean
+    ProfileService profileService;
+
+    @MockBean
+    WishlistService wishlistService;
+
+    @MockBean
+    WatchModeService watchModeService;
+
+    @MockBean
+    MovieService movieService;
+
+    @Test
+    void clientsTest() {
         String query = """
                 query{
                     clients{
@@ -41,7 +51,7 @@ class GraphqlControllerTest {
     }
 
     @Test
-    void clientByIdTest(){
+    void clientByIdTest() {
         String query = """
                 query clientById($id: ID){
                   clientById(id: $id){
@@ -71,7 +81,7 @@ class GraphqlControllerTest {
     }
 
     @Test
-    void profilesTest(){
+    void profilesTest() {
         String query = """
                 query {
                   profiles{
