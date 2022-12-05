0a5ddeb1e375936fb6b0f1bfccfc7a64b5b7e949
ben-natan <theadrien2008@gmail.com>
2022-12-05 02:22:31 -0500

    chore: clear warning

--- src/test/java/coms/w4156/moviewishlist/controllers/GraphqlControllerTest.java
+++ src/test/java/coms/w4156/moviewishlist/controllers/GraphqlControllerTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 
